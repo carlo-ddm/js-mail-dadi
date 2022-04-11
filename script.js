7ed46e28f5f3a16b31721425a93c0a430b2bbdf0
carlo-ddm <carloduminuco90@gmail.com>
2022-04-11 16:09:03 +0200

    struttura / ragionamento logico "dadi"

---
+++ script.js
@@ -0,0 +1,48 @@
+console.log('test');
+
+// const classe61 = ['Domenico Agostino',
+//   'Mihai Badia',
+//   'Riccardo Becattini',
+//   'Manuel Benini',
+//   'Bruno Bernardini',
+//   'Guido Buono',
+//   'Pierluigi Cancellaro',
+//   'Francesco Cataletto',
+//   'Stefano Cravotta',
+//   'Giovanni Crispino',
+//   'Giancarlo Croce',
+//   'Enrico De Stefano',
+//   'Silvia Dugato',
+//   'Carlo Duminuco Del Monte',
+//   'Francesco Ercoli',
+//   'Luca Gaddini',
+//   'Marco Gagliano',
+//   'Cristina Gasperini',
+//   'Gabriele Giusti',
+//   'Chiara Grillo',
+//   'Manuela Ienuso',
+//   'Michele Iliescu',
+//   'Ketevan Jorjoliani',
+//   'Renato Lami',
+//   'Davide Manciucca',
+//   'Emanuele Mastronardi',
+//   'Luca Mazzarini',
+//   'Daniele Monteleone',
+//   'Salvatore Olivieri',
+//   'Leonardo Pellegrini',
+//   'Agostino Piquè',
+//   'Davide Pizzolla',
+//   'Antonio Pollo',
+//   'Ivan Russo',
+//   'Amanjit Singh',
+//   'Sebastiano Visco'
+//   ];
+
+/*
+Dadi
+1) chiedere all'utente di estrarre un numero da 1 a 6;
+2) estrazione di un numero random per pcPlayer;
+3) confrontare i due numeri;
+4) dichiarare il vincitore;
+5) stampare a video;
+*/
