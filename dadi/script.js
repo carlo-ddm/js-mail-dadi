a9a8cdfd5867feec8eeb41cbda5e59e6bd11ac45
carlo-ddm <carloduminuco90@gmail.com>
2022-04-11 17:32:30 +0200

    esercizio dadi

---
+++ dadi/script.js
@@ -0,0 +1,40 @@
+console.log('test');
+
+// Dadi
+const max = 6
+
+const numberPlayer = Math.ceil (Math.random() * 6)
+console.log(numberPlayer);
+
+const numberPc = Math.ceil (Math.random() * 6)
+console.log(numberPc);
+
+const result = document.getElementById('result')
+console.log(result);
+
+if (numberPlayer < numberPc){
+  result.innerHTML = 'Hai perso'
+} else if (numberPlayer > numberPc){
+  result.innerHTML = 'Hai vinto'
+} else {
+  result.innerHTML = 'Pareggio'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
